9a8d21d9f16b5b3e4dd095c52ab4c8d823000639
forrestLyman <forrestlyman@gmail.com>
2018-01-15 09:22:44 -0800

    removes gulp watch tasks

--- gulp/tasks/watchify.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const browserifyTask = require('./browserify');
-
-gulp.task('watchify', callback => {
-    // Start browserify task with devMode === true
-    browserifyTask(callback, true);
-});
